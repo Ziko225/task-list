7b57d3450be7fd15e021c7ae9af886868dbe6d68
Nazar Bychyk <nazar1bychyk@gmail.com>
2022-04-27 19:51:45 +0200

    js update (adding new task)

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
             check: true,
         },
         {
-            content: "jakis tekst",
+            content: "jakis tekst l Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas recusandae delectus natus necessitatibus. Maiores reiciendis natus fugiat rem nesciunt optio voluptas, quam laudantium recusandae asperiores odit totam nemo eius pariatur! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit, sint pariatur. Odio, adipisci iste deserunt dolores perspiciatis modi consequatur labore vel ipsa? Maiores quos quia commodi ut, iure assumenda voluptatibus.",
             check: false,
         },
     ];
@@ -31,40 +31,22 @@
     };
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form")
+
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            const newTaskContent = document.querySelector(".js-taskInput").value.trim();
+            if(newTaskContent === "") {
+                return;
+            }
+
+            tasks.push({
+                content: newTaskContent,
+            })
+            render();
+        })
     };
-    console.log("hello  world");
     init();
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const formElement = document.querySelector(".js-form")
-const taskInputElement = document.querySelector(".js-taskInput")
+};
